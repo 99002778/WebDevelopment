b605a60a7c6d96ddde9cbb50aab7ef0fed30b0e1
99002778 <72496315+99002778@users.noreply.github.com>
2020-10-16 08:56:56 +0530

    Add files via upload

---
+++ EMS-Assignment2/Scripts/common.js
@@ -0,0 +1,12 @@
+function $1(id){
+    return document.getElementById(id)
+}
+function $hide(id){
+    $1(id).style.display = "none"
+}
+function $show(id){
+    $1(id).style.display = "inline-block"
+}
+function $mapEvent(className, callbackFn){
+    
+}
